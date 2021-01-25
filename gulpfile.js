ad967c6e878b562db56240411da1901373e99d74
Raul Arellano Guevara <raularellanoguevara@gmail.com>
2021-01-24 22:21:16 -0600

    correccion de archivo gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,66 @@
-var gulp= require('gulp');
-var browserSync = require('browser-sync').create();
-var sass= require('gulp-sass');
+'use strict';
 
-// Static Server + watching scss/html files
-gulp.task('server', ['sass'], function() {
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss =  require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
-    browserSync.init({
-        server: "./"
+
+gulp.task('sass', function(){
+    gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function(){
+    gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function(){
+    var files = ['*.html', './css/*.css', './img/*.{png, jpg, gif}', './js/*.js']
+    browserSync.init(files, {
+        server:{
+            baseDir: './'
+        }
     });
-
-    gulp.watch("./css/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("./css/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./css"))
-        .pipe(browserSync.stream());
+gulp.task('default', ['browser-sync'], function(){
+    gulp.start('sass:watch');
 });
 
-gulp.task('default', ['server']);
+gulp.task('clean', function(){
+    return del(['dist']);
+});
+
+gulp.task('imagemin', function(){
+    return gilp.src('./images/*.{png, jpg, jpeg, gif}')
+        .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('usemin', function(){
+    return gulp.src('./*.html')
+            .pipe(flatmap(function(stream, file){
+                return stream
+                        .pipe(usemin({
+                            css: [rev()],
+                            html: [function(){return htmlmin({collapseWhitespace: true})}],
+                            js: [uglify(), rev()],
+                            inlinejs: [uglify()],
+                            inlinecss: [cleanCss(), 'concat']
+                        }))
+            }))
+            .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', function(){
+    gulp.start('copyfonts', 'imagemin', 'usemin');
+});
